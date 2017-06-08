2a1b5ff86939d22f4f48a19ae49cf6b669f4fd89
Hai Pham <viethai.pham@holmusk.com>
2017-06-08 12:02:48 +0800

    Added stationary direction

--- src/main/java/org/swiften/xtestkitcomponents/direction/Unidirection.java
+++ src/main/java/org/swiften/xtestkitcomponents/direction/Unidirection.java
@@ -14,15 +14,18 @@ public enum Unidirection {
     UP_DOWN,
     DOWN_UP,
     LEFT_RIGHT,
-    RIGHT_LEFT;
+    RIGHT_LEFT,
+    NONE;
 
     /**
-     * Get {@link Unidirection#UP_DOWN} or {@link Unidirection#DOWN_UP},
-     * depending whether top-down is true.
+     * Get {@link #UP_DOWN} or {@link #DOWN_UP}, depending whether top-down
+     * is true.
      * @param topDown {@link Boolean} value. If this is true, return
      *                {@link Unidirection#UP_DOWN}, and
      *                {@link Unidirection#DOWN_UP} otherwise.
      * @return {@link Unidirection} instance.
+     * @see #DOWN_UP
+     * @see #UP_DOWN
      */
     @NotNull
     public static Unidirection vertical(boolean topDown) {
