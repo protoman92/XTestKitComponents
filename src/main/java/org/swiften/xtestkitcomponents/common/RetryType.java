fb7bcc8ced26e0f0e59988a120328850f19a39c1
Hai Pham <viethai.pham@holmusk.com>
2017-06-16 13:04:31 +0800

    Added configuration options for default interface methods

--- src/main/java/org/swiften/xtestkitcomponents/common/RetryType.java
+++ src/main/java/org/swiften/xtestkitcomponents/common/RetryType.java
@@ -1,6 +1,7 @@
 package org.swiften.xtestkitcomponents.common;
 
 import org.jetbrains.annotations.NotNull;
+import org.swiften.xtestkitcomponents.util.Constants;
 
 /**
  * Created by haipham on 3/23/17.
@@ -16,8 +17,9 @@ public interface RetryType {
      * Use this retry count if we are running an operation that is not expected
      * to throw {@link Exception}.
      * @return {@link Integer} value.
+     * @see Constants#defaultRetries()
      */
     default int retries() {
-        return 3;
+        return Constants.defaultRetries();
     }
 }
